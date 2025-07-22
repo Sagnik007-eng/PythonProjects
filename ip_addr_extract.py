bea9051502151b766b2274fc7afb486b8ffaddaa
Sagnik Bhattacharya <bhattacharya.sagnik@gmail.com>
2025-07-22 20:54:30 +0530

    IP address

---
+++ ip_addr_extract.py
@@ -0,0 +1,21 @@
+#Need to exract the IP addresses from the below string
+
+
+interface = '''wlo1: flags=4163<UP,BROADCAST,RUNNING,MULTICAST>  mtu 1500
+        inet 192.168.0.105  netmask 255.255.255.0  broadcast 192.168.0.255
+        inet6 fe80::6ec6:fea5:4a08:33ae  prefixlen 64  scopeid 0x20<link>
+        ether 14:13:33:ee:9c:1b  txqueuelen 1000  (Ethernet)
+        RX packets 1007942  bytes 902567350 (902.5 MB)
+        RX errors 0  dropped 0  overruns 0  frame 0
+        TX packets 531217  bytes 158518152 (158.5 MB)
+        TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0'''
+
+
+l1=interface.split()
+
+
+
+l4=[i for i in l1 if i.__contains__('.') or i.__contains__(':')]
+l5=[i for i in l4 if i.count('.')>1 or i.count(':')>1]
+
+print(l5)
